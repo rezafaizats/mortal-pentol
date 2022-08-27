8320d414f070da8dc1aadf7306c6456bc3802340
Reza Faiz <rezafaizats@gmail.com>
2022-08-28 01:33:43 +0700

    Polish main menu

--- Assets/Scripts/MainMenuSceneHandler.cs
+++ Assets/Scripts/MainMenuSceneHandler.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class MainMenuSceneHandler : MonoBehaviour
 {
+
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private Image bgImage;
+
+    private void Start() {
+        ChangeBGColor();
+    }
 
     public void QuitApps() {
         Application.Quit();
@@ -12,6 +21,29 @@ public class MainMenuSceneHandler : MonoBehaviour
 
     public void LoadScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void PlaySound(AudioClip sfx) {
+        audioSource.clip = sfx;
+        audioSource.Play();
+    }
+
+    public void ChangeBGColor() {
+
+        bgImage.DOColor(RollColorSprite(), 10f).OnComplete( () => {
+            ChangeBGColor();
+        });
+
+    }
+
+    public Color RollColorSprite() {
+
+        float red = Mathf.Clamp(Random.Range(0.2f, 0.8f), 0, 255);
+        float green = Mathf.Clamp(Random.Range(0.2f, 0.8f), 0, 255);
+        float blue = Mathf.Clamp(Random.Range(0.2f, 0.8f), 0, 255);
+
+        Color rgbColor = new Color(red, green, blue, 1f);
+        return rgbColor;
     }
 
 }
